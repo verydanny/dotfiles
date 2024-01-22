6fb6113ec2b946df5e2ffaada0d90e16830c1e58
Daniil Veremchuk <daniel@verydanny.com>
2024-01-22 17:36:31 +0000

    updated script

--- scripts/backupPalWorld.sh
+++ scripts/backupPalWorld.sh
@@ -8,8 +8,26 @@ function backup_palworld() {
     mkdir -p $BACKUP_DIR
   fi
 
+  # Backup palworld dir
   cd $PALWORLD_DIR
-  tar -cf $BACKUP_DIR/archive_$(date +%Y%m%d%H%M).tar "Pal"
+  tar -cf - "Pal" | lz4 > "$BACKUP_DIR/archive_$(date +%Y%m%d%H%M).tar.lz4"
+
+  # Delete backups older than 3 hours, we have limited space
+  find "$BACKUP_DIR" -type f -mmin +180 -exec rm {} \;
 }
 
+function restore_backup() {
+  
+  latest_backup=$(ls -t $BACKUP_DIR/*.tar.lz4 | head -n 1)
+
+  if [[ -f "$latest_backup" ]]; then
+    lz4 -d "$latest_backup" | tar -xf - -C "$PALWORLD_DIR"
+    echo "Restoration complete from backup: $latest_backup"
+  else
+    echo "No backup file found in $BACKUP_DIR"
+  fi
+}
+
+# restore_backup
 backup_palworld
+
